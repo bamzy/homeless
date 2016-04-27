2474b26da6f5ba2448a29c6fa0bf5a3f8cb38875
Bamdad A <bamdad.ag@gmail.com>
2016-04-27 12:49:00 -0600

    first commit

---
+++ app/Http/routes.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/WalkingRoute',function(){
+   $walkingRoute = App\WalkingRoute::all();
+});
+//Route::get('/hello/{name}', function ($name) {
+//    echo ('you are in hello '. $name);
+//});
+//
+//Route::post('test',function(){
+//   echo 'POSTED';
+//});
+//
+//
+//Route::get('test',function(){
+//    echo '<form action="test" method="POST">';
+//    echo '<input type="submit" value="Send">';
+//    echo '<input type="hidden" name="_token" value="' . csrf_token() . '">';
+//    echo '<input type="hidden" name="_method" value="DELETE">';
+//    echo '</form>';
+//});
+//
+//
+//Route::put('test',function(){
+//    echo 'PUT';
+//});
+//
+//Route::delete('test',function(){
+//    echo 'DELETE';
+//});
---
+++ app/WalkingRoute.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CIRCA
+ * Date: 26/04/2016
+ * Time: 10:48 PM
+ */
+
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+class WalkingRoute extends Model
+{
+
+}
